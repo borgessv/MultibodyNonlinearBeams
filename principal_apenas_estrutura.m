1523c6f33c95ee63a1bc37f211afd58dac6b005e
borgessv <99821785+borgessv@users.noreply.github.com>
2022-02-16 13:51:35 -0300

    Add existing file

---
+++ principal_apenas_estrutura.m
@@ -0,0 +1,23 @@
+clear all;
+addpath('multibody');
+
+%% carrega parametros da asa
+wing = highlyflex; % Patil --> Hodges
+
+
+%% inicializa e grava o modelo
+nrb = 15; %numero de corpos rigidos
+%matrizes_multibody = multibody(wing,nrb,"coupled"); % a versao coupled
+%acopla torcao e flexao e eh bem mais lenta para calcular a cinematica!
+matrizes_multibody = multibody(wing,nrb);
+
+%%
+% cálculo de equilíbrio
+func =@(q) matrizes_multibody.K*q - matrizes_multibody.B_gravfun(q);% 
+Xeq = fsolve(func,zeros(2*nrb,1)); 
+
+h = matrizes_multibody.pos_(Xeq);
+h = [0;h(1:nrb);0;h(nrb+1:2*nrb);0;h(2*nrb+1:3*nrb)];
+
+figure;plot3(h(1:nrb+1), h((nrb+2):(2*nrb+2)), h((2*nrb+3):end)); axis equal;
+

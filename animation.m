ccdf52bce5edf534897bc5734446544c5b3d7e70
borgessv <99821785+borgessv@users.noreply.github.com>
2022-03-24 09:34:33 -0300

    add existing file

--- animation.m
+++ animation.m
@@ -2,61 +2,46 @@ clear
 close all
 clc
 
-n_element = 20;
 n_frame = 100;
 test1 = linspace(0,pi/6,20).';
 test2 = linspace(pi/6,pi/4,10).';
 test3 = linspace(pi/4,pi/3,10).';
-q1 = 0*[0*linspace(0,0.1,20).';-0*test1;0.5*test1;0*test1];
-q2 = 0*[0*linspace(0,0.1,20).';-0*test1;0.5*test1;0*test1];
-q3 = 0*[0*linspace(0,0.1,10).';-0*test1;0*test1;0*test1];
-q4 = 0*[0*linspace(0,0.1,10).';-0*test1;0*test1;0*test1];
-q5 = 0*[0*rand(20,1);-2*test1;-test1;0.5*test1];
-q6 = 0*[0*rand(10,1);-2*test2;-test2;0.5*test2];
-q7 = 0*[0*rand(10,1);-2*test3;-test3;0.5*test3];
-q8 = 0*[0*rand(20,1);2*test1;-test1;-0.5*test1];
-q9 = 0*[0*rand(10,1);2*test2;-test2;-0.5*test2];
-q10 = 0*[0*rand(10,1);2*test3;-test3;-0.5*test3];
-
-Q(1).q = q1;
-Q(2).q = q2;
-Q(3).q = q3;
-Q(4).q = q4;
-Q(5).q = q5;
-Q(6).q = q6;
-Q(7).q = q7;
-Q(8).q = q8;
-Q(9).q = q9;
-Q(10).q = q10;
+test4 = linspace(0,pi/6,10).';
 t = sin(linspace(0,2*pi,n_frame));
-v = VideoWriter('video.avi');
-v.Quality = 100;
-v.FrameRate = 10;
-open(v);
-f = figure(1);
-f.WindowState = 'maximized';
+
+% % Video settings:
+% v = VideoWriter('video.avi');
+% v.Quality = 100;
+% v.FrameRate = 50;
+% open(v);
+
+% Figure settings:
+f = figure('visible','off');
+% f.WindowState = 'maximized';
+set(gcf, 'Position',  [250, 42, 750, 645])
+set(gcf,'color','w');
+
+% Gif settings:
+filename = 'simulation.gif';
+
+progressbar('loading animation...');
+frame = struct('cdata',0,'colormap',0);
+im = cell(n_frame,1);
 for i = 1:n_frame
-    plot_structure_test(Q,'beams.mat','DisplayElements','DisplayUndeformed')
-    hold off
-    xlim([-25 15])
-    ylim([-35 35])
-    zlim([-20 20])
-    view(45,25)
-    %drawnow
-    
-    frame(i) = getframe(gcf);
-    writeVideo(v,frame(i));
-   
+%     t1 = tic;
     q1 = [t(i)*0*linspace(0,0.2,10).';t(i)*0*test2;t(i)*0.25*test2;abs(t(i)*0*test2)];
     q2 = [t(i)*0*linspace(0,0.2,20).';t(i)*0*test1;t(i)*0.25*test1;abs(t(i)*0*test1)];
     q3 = [t(i)*0*linspace(0,0.2,10).';t(i)*0*test2;t(i)*0.25*test3;abs(t(i)*0*test2)];
     q4 = [t(i)*0*linspace(0,0.2,10).';t(i)*0*test2;t(i)*0.25*test2;abs(t(i)*0*test2)];
-    q5 = [t(i)*0.5*linspace(0,0.2,20).';t(i)*-2*test1;t(i)*-test1;abs(t(i)*0.5*test1)];
-    q6 = [t(i)*0.5*linspace(0,0.2,10).';t(i)*-2*test2;t(i)*-test2;abs(t(i)*0.5*test2)];
-    q7 = [t(i)*0.5*linspace(0,0.2,10).';t(i)*-2*test3;t(i)*-test3;abs(t(i)*0.5*test3)];
-    q8 = [t(i)*0.5*linspace(0,0.2,20).';t(i)*2*test1;t(i)*-test1;-abs(t(i)*0.5*test1)];
-    q9 = [t(i)*0.5*linspace(0,0.2,10).';t(i)*2*test2;t(i)*-test2;-abs(t(i)*0.5*test2)];
-    q10 = [t(i)*0.5*linspace(0,0.2,10).';t(i)*2*test3;t(i)*-test3;-abs(t(i)*0.5*test3)];
+    q5 = [t(i)*linspace(0,0.3,20).';-2*t(i)*test1;t(i)*-test1;abs(t(i)*0.5*test1)];
+    q6 = [t(i)*linspace(0,0.3,10).';-2*t(i)*test2;t(i)*-test2;abs(t(i)*0.5*test2)];
+    q7 = [t(i)*linspace(0,0.3,10).';-2*t(i)*test3;t(i)*-test3;abs(t(i)*0.5*test3)];
+    q8 = [t(i)*linspace(0,0.3,20).';2*t(i)*test1;t(i)*-test1;-abs(t(i)*0.5*test1)];
+    q9 = [t(i)*linspace(0,0.3,10).';2*t(i)*test2;t(i)*-test2;-abs(t(i)*0.5*test2)];
+    q10 = [t(i)*linspace(0,0.3,10).';2*t(i)*test3;t(i)*-test3;-abs(t(i)*0.5*test3)];
+    q11 = [t(i)*linspace(0,0.2,10).';-t(i)*test4;-t(i)*test4;abs(t(i)*0.5*test4)];
+    q12 = [t(i)*linspace(0,0.2,10).';t(i)*test4;-t(i)*test4;-abs(t(i)*0.5*test4)];
+    q13 = [t(i)*linspace(0,0.1,10).';t(i)*0*test4;abs(0*t(i)*test4);t(i)*test4];
     Q(1).q = q1;
     Q(2).q = q2;
     Q(3).q = q3;
@@ -67,9 +52,35 @@ for i = 1:n_frame
     Q(8).q = q8;
     Q(9).q = q9;
     Q(10).q = q10;
+    Q(11).q = q11;
+    Q(12).q = q12;
+    Q(13).q = q13;
+    
+    plot_structure_test(Q,'beams.mat','DisplayElements','DisplayUndeformed')
+    hold off
+    xlim([-25 15])
+    ylim([-35 35])
+    zlim([-25 25])
+    view(60,20)
+    frame(i) = getframe(gcf);
+
+%     % Creating .avi video:
+%     writeVideo(v,frame(i));
+
+    % Creating .gif:
+    im{i} = frame2im(frame(i));
+    [A,map] = rgb2ind(im{i},256);
+    if i == 1
+        imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',0);
+    elseif any(i == 0:4:n_frame)
+        imwrite(A,map,filename,'gif','WriteMode','append','DelayTime',0);
+    end
+
+    progressbar(i/n_frame*100);
+    %waitbar(i/n_frame)
+%     t2 = toc(t1);
+%     t_rem = n_frame*(t2) - i*(t2);
+%     fprintf('Remaining time: %.2f',t_rem)
 end
-close (v)
-%%
-% fig = figure(2);
-% set(gcf, 'Position',  [250, 50, 800, 600])
-% movie(fig,frame,10,20)
+% close(v)
+web('simulation.gif')
--- create_beam.m
+++ create_beam.m
@@ -9,20 +9,23 @@ m0_beam = beam_properties(5,:);
 m1_beam = beam_properties(6,:);
 c0_beam = beam_properties(7,:);
 c1_beam = beam_properties(8,:);
-yCM_beam = beam_properties(9,:);
-zCM_beam = beam_properties(10,:);
-Lambda_beam_deg = beam_properties(11,:);
-Gamma_beam_deg = beam_properties(12,:);
-iw_beam_deg = beam_properties(13,:);
-connectivity_beam = beam_properties(14,:);
-connection_point_beam = beam_properties(15,:);
+xCM_beam = beam_properties(9,:);
+yCM_beam = beam_properties(10,:);
+zCM_beam = beam_properties(11,:);
+Lambda_beam_deg = beam_properties(12,:);
+Gamma_beam_deg = beam_properties(13,:);
+AoI_beam_deg = beam_properties(14,:);
+connectivity_beam = beam_properties(15,:);
+connection_point_beam = beam_properties(16,:);
 
 n_beam = length(ID_beam);
-beam(1:n_beam) = struct('n_element',0,'L',0,'L_element',0,...
-    'C_i0',zeros(3),'yCM',0,'zCM',0,'m',0,'c',0,...
+beam(1:n_beam) = struct('Xsection','','n_element',0,'L',0,'L_element',0,...
+    'C_i0',zeros(3),'yCM',0,'zCM',0,...
+    'Lambda_deg',0,'Gamma_deg',0,'AoI_deg',0,'m',0,'c',0,...
     'connectivity',0,'connection_point',0,...
+    'connection_element',0,'connection_element_point',0,...
     'element',struct('r0',zeros(3,1),'r1',zeros(3,1),...
-    'x0',0,'x1',0,'m',0,'c',0));
+    'x0_element',0,'x1_element',0,'m',0,'c',0));
 
 syms x
 for i_beam = 1:n_beam
@@ -30,25 +33,38 @@ for i_beam = 1:n_beam
     beam(i_beam).n_element = n_element_beam(i_beam);
     beam(i_beam).L = L_beam(i_beam);
     beam(i_beam).L_element = beam(i_beam).L/beam(i_beam).n_element;
-    beam(i_beam).C_i0 = DCM(1,deg2rad(iw_beam_deg(i_beam)))*DCM(2,deg2rad(Gamma_beam_deg(i_beam)))*DCM(3,deg2rad(Lambda_beam_deg(i_beam)));
+    beam(i_beam).Lambda_deg = Lambda_beam_deg(i_beam);
+    beam(i_beam).Gamma_deg = Gamma_beam_deg(i_beam);
+    beam(i_beam).AoI_deg = AoI_beam_deg(i_beam);
+    beam(i_beam).xCM = xCM_beam(i_beam);
     beam(i_beam).yCM = yCM_beam(i_beam);
     beam(i_beam).zCM = zCM_beam(i_beam);
+    beam(i_beam).C_i0 = DCM(1,deg2rad(beam(i_beam).AoI_deg))*DCM(2,deg2rad(beam(i_beam).Gamma_deg))*DCM(3,deg2rad(beam(i_beam).Lambda_deg));
     beam(i_beam).m = m0_beam(i_beam) + m1_beam(i_beam)*x;
     beam(i_beam).c = c0_beam(i_beam) + c1_beam(i_beam)*x;
     beam(i_beam).connectivity = connectivity_beam(i_beam);
     beam(i_beam).connection_point = connection_point_beam(i_beam);
 
     for i_element = 1:beam(i_beam).n_element
-        if i_element == 1
+        if i_element == 1 && isnan(beam(i_beam).connectivity) && isnan(beam(i_beam).connection_point)
+            beam(i_beam).element(i_element).r0 = [0;0;0];
             beam(i_beam).element(i_element).x0_element = 0;
-        else 
-        end
-        beam(i_beam).element(i_element).x1_element = beam(i_beam).element(i_element).x0_element + beam(i_beam).L_element;
-        if i_element < beam(i_beam).n_element
-            beam(i_beam).element(i_element+1).x0_element = beam(i_beam).element(i_element).x1_element;
+            beam(i_beam).element(i_element).x1_element = beam(i_beam).element(i_element).x0_element + beam(i_beam).L_element;
+        elseif i_element == 1 && isnan(beam(i_beam).connectivity) && ~isnan(beam(i_beam).connection_point)
+            beam(i_beam).element(i_element).r0 = beam(i_beam).C_i0.'*[beam(i_beam).connection_point;0;0];
+            beam(i_beam).element(i_element).x0_element = 0;
+            beam(i_beam).element(i_element).x1_element = beam(i_beam).element(i_element).x0_element + beam(i_beam).L_element;
+        elseif i_element == 1 && ~isnan(beam(i_beam).connectivity)
+            beam(i_beam).element(i_element).r0 = beam(beam(i_beam).connectivity).C_i0.'*(beam(beam(i_beam).connectivity).C_i0*beam(beam(i_beam).connectivity).element(1).r0 + [beam(i_beam).connection_point;0;0]);
+            beam(i_beam).element(i_element).x0_element = 0;
+            beam(i_beam).element(i_element).x1_element = beam(i_beam).element(i_element).x0_element + beam(i_beam).L_element;
         else
+            beam(i_beam).element(i_element).r0 = beam(i_beam).element(i_element-1).r1;
+            beam(i_beam).element(i_element).x1_element = beam(i_beam).element(i_element).x0_element + beam(i_beam).L_element;
+
+
         end
-        if i_beam ~=1
+        if i_beam ~= 1
             for i = 1:beam(beam(i_beam).connectivity).n_element
                 if beam(i_beam).connection_point >= beam(beam(i_beam).connectivity).element(i).x0_element && beam(i_beam).connection_point <= beam(beam(i_beam).connectivity).element(i).x1_element
                     beam(i_beam).connection_element = i;
@@ -56,27 +72,18 @@ for i_beam = 1:n_beam
                 else
                 end
             end
+        else
         end
 
-            if i_element == 1 && isnan(beam(i_beam).connectivity) && isnan(beam(i_beam).connection_point)
-                beam(i_beam).element(i_element).r0 = [0;0;0];
-                beam(i_beam).element(i_element).x0 = 0;
-            elseif i_element == 1 && isnan(beam(i_beam).connectivity) && ~isnan(beam(i_beam).connection_point)
-                beam(i_beam).element(i_element).r0 = beam(i_beam).C_i0.'*[beam(i_beam).connection_point;0;0];
-                beam(i_beam).element(i_element).x0 = 0;
-            elseif i_element == 1 && ~isnan(beam(i_beam).connectivity)
-                beam(i_beam).element(i_element).r0 = beam(beam(i_beam).connectivity).C_i0.'*(beam(beam(i_beam).connectivity).C_i0*beam(beam(i_beam).connectivity).element(1).r0 + [beam(i_beam).connection_point;0;0]);
-                beam(i_beam).element(i_element).x0 = 0;
-            else
-                beam(i_beam).element(i_element).r0 = beam(i_beam).element(i_element-1).r1;
-                beam(i_beam).element(i_element).x0 = beam(i_beam).element(i_element-1).x1;
-            end
         beam(i_beam).element(i_element).r1 = beam(i_beam).C_i0.'*(beam(i_beam).C_i0*beam(i_beam).element(i_element).r0 + [beam(i_beam).L_element;0;0]);
-        beam(i_beam).element(i_element).x1 = beam(i_beam).element(i_element).x0 + beam(i_beam).L_element;
 
-        beam(i_beam).element(i_element).m = double(vpaintegral(beam(i_beam).m,beam(i_beam).element(i_element).x0,beam(i_beam).element(i_element).x1));
-        beam(i_beam).element(i_element).c = double(mean([subs(beam(i_beam).c,x,beam(i_beam).element(i_element).x1),subs(beam(i_beam).c,x,beam(i_beam).element(i_element).x0)]));
-        
+        beam(i_beam).element(i_element).m = double(vpaintegral(beam(i_beam).m,beam(i_beam).element(i_element).x0_element,beam(i_beam).element(i_element).x1_element));
+        beam(i_beam).element(i_element).c = double(mean([subs(beam(i_beam).c,x,beam(i_beam).element(i_element).x1_element),subs(beam(i_beam).c,x,beam(i_beam).element(i_element).x0_element)]));
+
+        if i_element ~= beam(i_beam).n_element
+            beam(i_beam).element(i_element+1).x0_element = beam(i_beam).element(i_element).x1_element;
+        else
+        end
     end
-    save beams.mat beam 
+    save beams.mat beam
 end
